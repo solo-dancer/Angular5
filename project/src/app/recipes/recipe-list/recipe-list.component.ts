c16aa698d398cf83c9f3ccbdbb18a2ce99c8874c
AKHILESH VERMA <vermakhilesh@icloud.com>
2017-12-30 16:21:52 +0530

    updating stuff

---
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple test',
+      'https://sinamontales.files.wordpress.com/2009/11/dsc_0398.jpg'
+      )
+    ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
